9808ee2aa46cc9dd6dcc53e6bc6b411fb00ee8c2
Floppi <112648116+xf1op@users.noreply.github.com>
2026-06-27 09:46:25 +0200

    studio fix

--- Roblox-Blocklist.ps1
+++ Roblox-Blocklist.ps1
@@ -35,7 +35,6 @@ $domains = @(
     "gold.roblox.com"
     "lms.roblox.com"
     "metrics.roblox.com"
-    "pulsar.roblox.com"
     "realtime-signalr.roblox.com"
     "roblox-poc.global.ssl.fastly.net"
     "sc0.rbxcdn.com"
