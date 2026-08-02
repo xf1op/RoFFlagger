c32d4dbbf133526ad4dfc0e07253370348f0d94c
Floppi <112648116+xf1op@users.noreply.github.com>
2026-08-02 19:55:02 +0200

    Update Roblox-Blocklist.ps1

--- Roblox-Blocklist.ps1
+++ Roblox-Blocklist.ps1
@@ -1,94 +1,61 @@
 $hostsFile = "$env:SystemRoot\System32\drivers\etc\hosts"
 
-$domains = @(
-    "ams2-128-116-21-3.roblox.com"
-    "atl4-128-116-2-3.roblox.com"
-    "bom2-128-116-47-3.roblox.com"
-    "cdg2-128-116-13-3.roblox.com"
-    "dfw2-128-116-95-3.roblox.com"
-    "fra2-128-116-5-3.roblox.com"
-    "fra4-128-116-44-3.roblox.com"
-    "gru1-128-116-86-3.roblox.com"
-    "iad2-128-116-56-3.roblox.com"
-    "iad4-128-116-102-3.roblox.com"
-    "iad5-44-197-17-243.roblox.com"
-    "lax2-128-116-116-3.roblox.com"
-    "lax4-128-116-63-3.roblox.com"
-    "lga2-128-116-32-3.roblox.com"
-    "lhr4-128-116-31-3.roblox.com"
-    "mia2-128-116-127-3.roblox.com"
-    "mia4-128-116-45-3.roblox.com"
-    "nrt2-128-116-120-3.roblox.com"
-    "ord2-128-116-48-3.roblox.com"
-    "sea1-128-116-115-3.roblox.com"
-    "sin2-128-116-97-3.roblox.com"
-    "sin4-128-116-50-3.roblox.com"
-    "sin6-128-116-54-3.roblox.com"
-    "sin6c2-128-116-46-3.roblox.com"
-    "syd1-128-116-51-3.roblox.com"
-    "contacts.roblox.com"
-    "ecsv2.roblox.com"
-    "edge-term4-ams2.roblox.com"
-    "edge-term4-aws-lax.roblox.com"
-    "edge-term4-aws.roblox.com"
-    "edge-term4-bom2.roblox.com"
-    "edge-term4-dfw2.roblox.com"
-    "edge-term4-fra4.roblox.com"
-    "edge-term4-lga2.roblox.com"
-    "edge-term4-mia2.roblox.com"
-    "edge-term4-nrt2.roblox.com"
-    "edge-term4-rcc-atl1.roblox.com"
-    "edge-term4-rcc-iad2.roblox.com"
-    "edge-term4-rcc-iad4.roblox.com"
-    "edge-term4-rcc-mia4.roblox.com"
-    "edge-term4-rcc-ord2.roblox.com"
-    "edge-term4-rcc-sea1.roblox.com"
-    "edge-term4-rcc-sin6.roblox.com"
-    "edge-term4-sin4.roblox.com"
-    "edge-term4.roblox.com"
-    "ephemeralcounters.api.roblox.com"
-    "gameinternationalization.roblox.com"
-    "gold.roblox.com"
-    "lms.roblox.com"
-    "locale.roblox.com"
-    "metrics.roblox.com"
-    "notifications.roblox.com"
-    "o293668.ingest.us.sentry.io"
-    "pulsar.roblox.com"
-    "realtime-signalr.roblox.com"
-    "rewarded-video.roblox.com"
-    "roblox-poc.global.ssl.fastly.net"
-    "sc0.rbxcdn.com"
-    "sc0ak.rbxcdn.com"
-    "sc0aws.rbxcdn.com"
-    "sc0cfly.rbxcdn.com"
-    "sc0gcp.rbxcdn.com"
-    "sc0ns1.rbxcdn.com"
-    "sc0rcc.rbxcdn.com"
-    "silver.roblox.com"
-    "titanium.roblox.com"
-    "tracing.roblox.com"
-    "upload.crashes.rbxinfra.com"
-    "userads.api.roblox.com"
-    "usermoderation.roblox.com"
-    "voice.roblox.com"
+$url = "https://raw.githubusercontent.com/xf1op/RoFFlagger/refs/heads/main/BlockList.txt"
+
+$githubList = @(
+    (Invoke-WebRequest $url -TimeoutSec 10).Content -split "`r?`n" |
+    ForEach-Object { $_.Trim() } |
+    Where-Object { $_ -and $_ -notmatch "^#" }
 )
 
-$existingDomains = [System.Collections.Generic.HashSet[string]]::new([System.StringComparer]::OrdinalIgnoreCase)
+Write-Host "Reading hosts..."
 
-Get-Content $hostsFile | ForEach-Object {
-    if ($_ -match '^\s*(?:\d{1,3}(?:\.\d{1,3}){3}|::1)\s+([^\s#]+)') {
-        $null = $existingDomains.Add($matches[1])
+$hosts = @(Get-Content $hostsFile)
+
+$start = $hosts.IndexOf("# Roblox Blocklist")
+
+if ($start -eq -1) {
+
+    Write-Host "Creating Roblox Blocklist..."
+
+    $hosts += ""
+    $hosts += "# Roblox Blocklist"
+
+    $start = $hosts.Count - 1
+}
+
+$end = $hosts.Count
+
+for ($i = $start + 1; $i -lt $hosts.Count; $i++) {
+
+    if ($hosts[$i] -match "^#") {
+        $end = $i
+        break
     }
 }
-if ((Get-Content $hostsFile) -notcontains "# Roblox Blocklist") {
-    Add-Content $hostsFile "`r`n# Roblox Blocklist"
-        Start-Sleep -Milliseconds 750
+
+$currentBlock = @()
+
+if ($end -gt $start + 1) {
+    $currentBlock = $hosts[($start + 1)..($end - 1)]
 }
-foreach ($domain in $domains) {
-    if ($existingDomains.Add($domain)) {
-        Add-Content $hostsFile "0.0.0.0 $domain"
-        Start-Sleep -Milliseconds 750
-        echo Added $domain
-    }
+
+Write-Host "Syncing blocklist..."
+
+$newBlock = @($githubList | Sort-Object)
+
+$newHosts = @()
+
+$newHosts += $hosts[0..$start]
+
+$newHosts += $newBlock
+
+if ($end -lt $hosts.Count) {
+    $newHosts += $hosts[$end..($hosts.Count - 1)]
 }
+
+$newHosts = $newHosts | Select-Object -Unique
+
+$newHosts | Out-File -FilePath $hostsFile -Encoding ASCII
+
+Write-Host "Finished."
