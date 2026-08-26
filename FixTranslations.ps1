5d8896ccb6a9f48f6836168f34da8dad3cad773c
Floppi <112648116+xf1op@users.noreply.github.com>
2026-08-26 22:09:24 +0200

    Create FixTranslations.ps1

---
+++ FixTranslations.ps1
@@ -0,0 +1,32 @@
+$file = "$env:SystemRoot\System32\drivers\etc\hosts"
+
+$domainsToRemove = @(
+    "gameinternationalization.roblox.com"
+    "locale.roblox.com"
+)
+
+Write-Host "Checking hosts..."
+
+$foundLines = @()
+
+foreach ($domain in $domainsToRemove) {
+
+    $result = @(cmd /c "findstr /I /B ""0.0.0.0 $domain"" ""$file""")
+
+    if ($result) {
+        $foundLines += "0.0.0.0 $domain"
+    }
+}
+
+$content = @(Get-Content $file)
+
+$newContent = foreach ($line in $content) {
+
+    if ($foundLines -notcontains $line.Trim()) {
+        $line
+    }
+}
+
+$newContent | Out-File -FilePath $file -Encoding ASCII
+
+Write-Host "Removed, VC Games Fixed!"
